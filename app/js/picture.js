b7e4ae82a3af6c1efe133cba9a0263216432ef67
Jemma <pink.fwip@gmail.com>
2013-06-15 22:54:09 -0400

    Add tags & filtering.

--- app/js/picture.js
+++ app/js/picture.js
@@ -2,12 +2,21 @@ function PictureDisplayCtrl($scope){
   $scope.pictures = [
   {
     "name": "babybel",
-    "url": "http://www.cheese.com/media/img/cheese/babybel_160.jpg"
+    "url": "http://www.cheese.com/media/img/cheese/babybel_160.jpg",
+    "tags": [
+      {"name": "cheese"},
+      {"name": "delicious"},
+      {"name": "babybel"}
+    ]
   },
   {
     "name": "camembert",
-    "url": "http://www.cheese.com/media/img/cheese/camembert_france.jpg"
+    "url": "http://www.cheese.com/media/img/cheese/camembert_france.jpg",
+    "tags": [
+      {"name": "cheese"},
+      {"name": "delicious"},
+      {"name": "camembert"}
+    ]
   }
-
-  ];
-}
+  ]
+};
