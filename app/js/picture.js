6e1d04b88418c1aa0ad41c162440736f07381ac6
Jemma <pink.fwip@gmail.com>
2013-06-15 22:08:12 -0400

    Add angular.js; add view to view pictures.
    Currently the pictures are hardcoded to two pictures of cheese.
    This limitation may be removed in the future, if we hit our funding
    goals.

---
+++ app/js/picture.js
@@ -0,0 +1,13 @@
+function PictureDisplayCtrl($scope){
+  $scope.pictures = [
+  {
+    "name": "babybel",
+    "url": "http://www.cheese.com/media/img/cheese/babybel_160.jpg"
+  },
+  {
+    "name": "camembert",
+    "url": "http://www.cheese.com/media/img/cheese/camembert_france.jpg"
+  }
+
+  ];
+}
